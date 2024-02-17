f1ba366a809d0bc55e1f354b1c564eb2a51baddf
Shreyas K N <shreyas2499@gmail.com>
2024-02-16 20:33:29 -0500

    Updated allowed hosts

--- swe1_app/settings.py
+++ swe1_app/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = "django-insecure-%sx*(yo0_wcdwx&$bod#az^)c2z5_a_$3n*1v0zidu2o!sn_hn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-7dfhshsi.us-west-2.elasticbeanstalk.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['django-env.eba-7dfhshsi.us-west-2.elasticbeanstalk.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["Django-app-env.eba-nwwq6smg.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
